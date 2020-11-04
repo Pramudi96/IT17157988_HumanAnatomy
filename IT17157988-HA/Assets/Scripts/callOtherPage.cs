f1a5389b93c3a9a3eb880122035168f26c9cc554
Pramudi96 <lprarangi@gmail.com>
2020-11-04 17:03:46 +0530

    add Human Anatomy AR Mobile Application

---
+++ IT17157988-HA/Assets/Scripts/callOtherPage.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class callOtherPage : MonoBehaviour
+{
+
+    public void changePageScene(string scenename){
+		
+		
+	Application.LoadLevel(scenename);
+    }
+}
